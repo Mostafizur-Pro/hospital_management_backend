e3bba69e1b9a72917a801e0987c6e84bc6ff987f
Mostafizur-Pro <mostafizur0195@gmail.com>
2024-08-12 19:43:04 +0600

    database create

---
+++ database/migrations/2024_08_12_134214_create_medical_tests_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medical_tests', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('patient_id');
+            $table->foreign('patient_id')->references('id')->on('patients');
+
+            
+            $table->string('test_name');
+            $table->text('results')->nullable();
+            $table->date('test_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medical_tests');
+    }
+};
